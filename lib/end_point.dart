34b1ea5c5240e8b52b69d4a6ff8dc75ba07cac6f
abdalla shiha <hossamkidr163@gmail.com>
2025-10-09 20:46:45 +0300

    added dio client and responsive helper and toast servises

--- lib/end_point.dart
+++ lib/end_point.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:kids_story_ai/core/utils/responsive_helper.dart';
+import 'package:kids_story_ai/core/utils/toast_service.dart';
 
 import 'config/routes/app_routes.dart';
 import 'config/routes/route_generator.dart';
@@ -10,6 +12,7 @@ class KidsStory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ResponsiveHelper.instance.init(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
@@ -18,10 +21,40 @@ class KidsStory extends StatelessWidget {
       localizationsDelegates: [...context.localizationDelegates],
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-home: Scaffold(),
+      home: HomeScreen(),
       initialRoute: "AppRoutes.splash",
 
       onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            TextButton(onPressed: () {
+              ToastService.success(context, 'Success Message');
+            }, child: Text('success')),
+            TextButton(onPressed: () {
+              ToastService.warning(context, 'Warning Message');
+            }, child: Text('warning')),
+            TextButton(onPressed: () {
+              ToastService.error(context, 'Error Message');
+            }, child: Text('error')),
+            TextButton(onPressed: () {
+              ToastService.info(context, 'Info Message');
+            }, child: Text('info')),
+          ],
+        ),
+      ),
     );
   }
 }
