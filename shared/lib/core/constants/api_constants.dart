d7e32856394c9a8aaaca6acdbe72acdd03bd40f8
abdalla shiha <hossamkidr163@gmail.com>
2025-10-26 03:07:55 +0300

    some cheanges in shared

--- shared/lib/core/constants/api_constants.dart
+++ shared/lib/core/constants/api_constants.dart
@@ -7,6 +7,7 @@ abstract class ApiConstants{
   static const String resetPasswordEndPoint = 'auth/resete';
   static const String categoryEndPoint = 'categories/';
   static const String storiesEndPoint = 'stories/';
+  static const String storiesByCategoryEndPoint = 'stories/category/';
   static const String getStoryEndPoint = 'stories';
 
   static const String tokenKey = 'token';
--- shared/lib/responses/story_response.dart
+++ shared/lib/responses/story_response.dart
@@ -1,26 +1,32 @@
 class StoryResponse {
   final int id;
   final String title;
-  final String description;
-  final String content;
   final String image;
-  final int categoryId;
+  final String? description;
+  final String content;
 
-  StoryResponse({
+  final int categoryId;
+  final String createdAd;
+
+  StoryResponse( {
     required this.id,
     required this.title,
+    required this.image,
     required this.description,
     required this.content,
-    required this.image,
     required this.categoryId,
+    required this.createdAd,
   });
 
   factory StoryResponse.fromJson(Map<String, dynamic> json) => StoryResponse(
-    id: json['ID'] as int,
+    id: json['id'] as int,
     title: json['title'] as String,
-    description: json['description'] as String,
-    content: json['content'] as String,
     image: json['image'] as String,
+    description: json['description']?.toString()??"No Descrrition",
+    content: json['content'] as String,
     categoryId: json['category_id'] as int,
+    createdAd: json['created_at'] as String,
   );
 }
+
+
